ae3216a3535951235c5e593084d95645cee0e6ae
Harsh Vardhan Singh <60401589+hvs42@users.noreply.github.com>
2021-07-04 00:00:19 +0530

    Update bridges_in_a_graph.cpp

--- bridges_in_a_graph.cpp
+++ bridges_in_a_graph.cpp
@@ -1,7 +1,13 @@
+// Coding Solution For MAR
+// Name: Harsh Vardhan Singh
+// University Roll No. : 16900219042
+
+
+// Problem :- Find Bridges in a graph
+
+
 #include<bits/stdc++.h>
 using namespace std;
-
-// dfs
 
 void dfs(int node, int parent, vector<int> &visited, vector<int> &tin, vector<int> &lowtin, 
                                                             int &timer, vector<int>adj[])
@@ -9,7 +15,7 @@ void dfs(int node, int parent, vector<int> &visited, vector<int> &tin, vector<in
     visited[node] = 1;
     tin[node] = lowtin[node] = timer++;
 
-    for(auto it : adj[node])        //iterate through adjacent nodes
+    for(auto it : adj[node])      
     {
         if(it == parent) continue;
 
@@ -34,10 +40,10 @@ void dfs(int node, int parent, vector<int> &visited, vector<int> &tin, vector<in
 
 int main()
 {
-    //first create graph
+    
 
     int n, m;
-    cin >> n >> m;   //no. of nodes and no. of edges respectively
+    cin >> n >> m;   
 
     vector<int> adj[n];
     for(int i=0; i<m; i++)
@@ -53,7 +59,6 @@ int main()
     vector<int> visited(n, 0);
     int timer=0;
 
-    // applying dfs
 
     for(int i=0; i<n; i++)
     {
@@ -65,46 +70,3 @@ int main()
     return 0;    
 }
 
-/*Note :-  This is correct programe all test cases from gfg have passed in this program
-
-Test Case 1 :-  Graph Link:- https://media.geeksforgeeks.org/wp-content/cdn-uploads/Bridge1.png
-
-        Input:-
-                5 5
-                1 0
-                0 2
-                2 1
-                0 3
-                3 4
-        Output:-
-                ( 3 -- 4 ) 
-                ( 0 -- 3 )
-
-Test Case 2 :-   Graph Link:- https://media.geeksforgeeks.org/wp-content/cdn-uploads/Bridge2.png
-
-        Input:-
-                4 3
-                0 1
-                1 2
-                2 3
-        Output:-
-                ( 2 -- 3 ) 
-                ( 1 -- 2 )
-                ( 0 -- 1 )
-
-Test Case 3 :-   Graph Link:- https://media.geeksforgeeks.org/wp-content/cdn-uploads/Bridge3.png
-
-        Input:-
-                7 8
-                0 1
-                1 2
-                2 0
-                1 3
-                1 4
-                1 6
-                3 5
-                4 5
-        Output:-
-                ( 1 -- 6 )
-
-*/
